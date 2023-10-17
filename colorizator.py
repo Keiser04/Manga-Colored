edbbc14bff1f7857a268632b98170b5aba88c6a5
Keiser04 <146493916+Keiser04@users.noreply.github.com>
2023-10-17 11:38:00 +0200

    colorizator.py

--- colorizator.py
+++ colorizator.py
@@ -7,7 +7,7 @@ from denoising.denoiser import FFDNetDenoiser
 from utils.utils import resize_pad
 
 class MangaColorizator:
-    def __init__(self, device, generator_path = 'networks/generator.zip', extractor_path = 'networks/extractor.pth'):
+    def __init__(self, device, generator_path = '/content/Manga-Colored/networks/generator.zip', extractor_path = 'networks/extractor.pth'):
         self.colorizer = Colorizer().to(device)
         self.colorizer.generator.load_state_dict(torch.load(generator_path, map_location = device))
         self.colorizer = self.colorizer.eval()
