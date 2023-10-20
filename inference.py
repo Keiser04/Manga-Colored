bc27231ad1014f173c5c4846477597004a2efae1
Keiser04 <146493916+Keiser04@users.noreply.github.com>
2023-10-20 23:04:48 +0200

    Update inference.py

--- inference.py
+++ inference.py
@@ -9,7 +9,6 @@ from colorizator import MangaColorizator
 
 def process_image(image, colorizator, args):
     colorizator.set_image(image, args.size, args.denoiser, args.denoiser_sigma)
-        
     return colorizator.colorize()
     
 def colorize_single_image(image_path, save_path, colorizator, args):
@@ -34,10 +33,15 @@ def colorize_images(target_path, colorizator, args):
         save_path = os.path.join(target_path, image_name)
         colorize_single_image(file_path, save_path, colorizator, args)
 
+        # Eliminar la imagen en blanco y negro después de procesarla si el booleano 'eliminar' es True
+        if args.eliminar:
+            os.remove(file_path)
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--path", required=True)
-    parser.add_argument("-o", "--output", default='colorized_images')  # Argumento para la opción de salida
+    parser.add_argument("-o", "--output", default='colorized_images')
+    parser.add_argument("--eliminar", dest='eliminar', action='store_true', help='Eliminar imágenes en blanco y negro después de procesarlas')
     parser.add_argument("-gen", "--generator", default='/content/Manga-Colored/networks/generator.zip')
     parser.add_argument("-ext", "--extractor", default='networks/extractor.pth')
     parser.add_argument('-g', '--gpu', dest='gpu', action='store_true')
@@ -46,6 +50,7 @@ def parse_args():
     parser.add_argument("-s", "--size", type=int, default=512)
     parser.set_defaults(gpu=True)
     parser.set_defaults(denoiser=True)
+    parser.set_defaults(eliminar=False)  # Valor predeterminado para la opción eliminar
     args = parser.parse_args()
     
     return args
@@ -61,7 +66,7 @@ if __name__ == "__main__":
     colorizer = MangaColorizator(device, args.generator, args.extractor)
     
     if os.path.isdir(args.path):
-        colorization_path = os.path.join(args.path, args.output)  # Utiliza el argumento de salida
+        colorization_path = os.path.join(args.path, args.output)
         if not os.path.exists(colorization_path):
             os.makedirs(colorization_path)
               
@@ -70,9 +75,13 @@ if __name__ == "__main__":
     elif os.path.isfile(args.path):
         split = os.path.splitext(args.path)
         
-        if split[1].lower() in ('.jpg', '.png', '.jpeg'):  # Arregla la coma a un punto en '.jpeg'
-            new_image_path = os.path.join(args.output, split[0] + '_colorized.png')  # Utiliza el argumento de salida
+        if split[1].lower() in ('.jpg', '.png', '.jpeg'):
+            new_image_path = os.path.join(args.output, split[0] + '_colorized.png')
             colorize_single_image(args.path, new_image_path, colorizer, args)
+            
+            # Eliminar la imagen en blanco y negro después de procesarla si el booleano 'eliminar' es True
+            if args.eliminar:
+                os.remove(args.path)
         else:
             print('Wrong format')
     else:
