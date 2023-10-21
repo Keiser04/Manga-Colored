a204a6a6e7080705ead66a34ee14f1ccd3751751
Keiser04 <146493916+Keiser04@users.noreply.github.com>
2023-10-21 12:15:55 +0200

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,40 +1,8 @@
 import os
 import argparse
-import sys
-
 import numpy as np
 import matplotlib.pyplot as plt
-
 from colorizator import MangaColorizator
-
-def process_image(image, colorizator, args):
-    colorizator.set_image(image, args.size, args.denoiser, args.denoiser_sigma)
-    return colorizator.colorize()
-
-def colorize_single_image(image_path, save_path, colorizator, args):
-    image = plt.imread(image_path)
-    colorization = process_image(image, colorizator, args)
-    plt.imsave(save_path, colorization)
-
-def colorize_images(target_path, colorizator, args):
-    images = os.listdir(args.path)
-    
-    for image_name in images:
-        file_path = os.path.join(args.path, image_name)
-        
-        if os.path.isdir(file_path):
-            continue
-        
-        name, ext = os.path.splitext(image_name)
-        if (ext != '.png'):
-            image_name = name + '.png'
-        
-        save_path = os.path.join(target_path, image_name)
-        colorize_single_image(file_path, save_path, colorizator, args)
-
-        # Eliminar la imagen en blanco y negro después de procesarla si la opción 'eliminar' es True
-        if args.eliminar:
-            os.remove(file_path)
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -44,18 +12,42 @@ def parse_args():
     parser.add_argument('-nd', '--no_denoise', dest='denoiser', action='store_false')
     parser.add_argument("-o", "--output", required=True, help='Ruta de salida para las imágenes coloreadas')
     args = parser.parse_args()
-    
     return args
+
+def colorize_single_image(image_path, output_path, colorizer, args):
+    image = plt.imread(image_path)
+    colorizer.set_image(image, args.size, not args.denoiser, args.size)  # No aplicamos denoise si args.denoiser es False
+    colorization = colorizer.colorize()
+    plt.imsave(output_path, colorization)
 
 if __name__ == "__main__":
     args = parse_args()
-    
+
     colorizer = MangaColorizator('cuda')  # Suponiendo que necesitas usar la GPU
-    
+
     if os.path.isdir(args.path):
         if not os.path.exists(args.output):
             os.makedirs(args.output)
-              
-        colorize_images(args.output, colorizer, args)
+
+        images = os.listdir(args.path)
+
+        for image_name in images:
+            file_path = os.path.join(args.path, image_name)
+
+            if os.path.isdir(file_path):
+                continue
+
+            name, ext = os.path.splitext(image_name)
+            if ext.lower() not in ['.png', '.jpg', '.jpeg']:
+                continue  # Saltar archivos que no son imágenes
+
+            save_path = os.path.join(args.output, image_name)
+            colorize_single_image(file_path, save_path, colorizer, args)
+
+            # Eliminar la imagen en blanco y negro después de procesarla si la opción 'eliminar' es True
+            if args.eliminar:
+                os.remove(file_path)
+
+        print("Procesamiento y eliminación de imágenes completados.")
     else:
         print('Ruta incorrecta o no es un directorio')
